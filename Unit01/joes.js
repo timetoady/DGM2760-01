2162478f6e6d7e1182a7ef87545bf70ea31bc266
ADAM ANDREASON <adamandreason@gmail.com>
2020-01-09 02:24:30 -0700

    fun code for if there is no prompt info

--- Unit01/joes.js
+++ Unit01/joes.js
@@ -1,3 +1,25 @@
-let mainArea = document.querySelector("main");
-let topHeader = document.querySelector("header");
-
+let yourName = prompt("Please give your first name: ");
+var Array1 = [
+    'stranger',
+    'friend',
+    'my dude',
+    'good fellow'
+]
+const noName = Array1[Math.floor(Math.random()*Array1.length)];
+function namer() {
+  if (yourName !== null && yourName !== "") {
+    return yourName;
+  } else {
+    return noName;
+  }
+}
+document.querySelector("#company").innerText = "Joe's Bed n' Breakfast";
+document.querySelector("header > h2").innerText =
+  "The Place to Sleep... and Eat Breakfast";
+let thedate = new Date();
+document.querySelector(
+  "#date"
+).innerText = `Today's date is ${thedate.toLocaleDateString()}.`;
+document.querySelector(
+  "#greeting"
+).innerText = `This is what I learned, ${namer()}:`;
