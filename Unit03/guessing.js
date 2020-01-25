fbc899191bd3c4cafd03cde0ef836c3134d206af
ADAM ANDREASON <adamandreason@gmail.com>
2020-01-25 02:40:29 -0700

    made button go buy when correct guess

--- Unit03/guessing.js
+++ Unit03/guessing.js
@@ -1,8 +1,8 @@
+//Company and slogan select
 document.querySelector("#company").innerText = "Guessing Game";
 document.querySelector("#slogan").innerText = "Better guess right!";
 
-//random number between 0 & 16
-
+//Definitions, for random selection, guesses, and created elements like results and award
 let max = 16;
 let min = 1;
 const number = Math.floor(Math.random() * (max - min)) + min;
@@ -15,7 +15,9 @@ let award = document.createElement("img");
 feedback.appendChild(results);
 attempts.appendChild(showGuess);
 prize.appendChild(award);
+let buttonStop = document.querySelector("#thebutton");
 
+//function to do guessing game
 function evalGuess() {
   gamerGuess = document.querySelector("#guess").value;
   const feedback = document.querySelector("#feedback");
@@ -30,12 +32,14 @@ function evalGuess() {
         award.alt = "First place!";
         results.innerText =
           "Congradulations! You've managed to guess the correct number on the first try. Your exploits will go down in history, and bards will sing songs of your excellence forever. Consider yourself a hero.";
+        buttonStop.remove();
         break;
       case 2:
       case 3:
         award.src = "/Unit03/assets/FirstPlacemdpi.png";
         award.alt = "First place!";
         results.innerText = "Correct! You the Champ!";
+        buttonStop.remove();
         break;
       case 4:
       case 5:
@@ -43,19 +47,23 @@ function evalGuess() {
         award.src = "/Unit03/assets/2ndPlacemdpi.png";
         award.alt = "Second place!";
         results.innerText = "Correct! Well done. Just short of greatness.";
+        buttonStop.remove();
         break;
       case 7:
       case 8:
       case 9:
         award.src = "/Unit03/assets/3rdPlacemdpi.png";
         award.alt = "Third place!";
-        results.innerText = "Correct! But you can do better!";
+        results.innerText =
+          "Correct! But you can do better! Refresh to try again!";
+        buttonStop.remove();
         break;
       default:
         award.src = "/Unit03/assets/Participationmdpi.png";
         award.alt = "Participation prize";
         results.innerText =
-          "Correct! But you must train harder to become a TRUE guesser!";
+          "Correct! But you must train harder to become a TRUE guesser! Refresh to try again!";
+        buttonStop.remove();
         break;
     }
   } else if (gamerGuess < 1 || gamerGuess > 15) {
@@ -69,7 +77,6 @@ function evalGuess() {
     results.innerText = "Too low! Please try again.";
     attempts.innerText = totalGuesses;
   } else {
-    totalGuesses++;
     console.log("I'm sorry, please try again.");
   }
 }
