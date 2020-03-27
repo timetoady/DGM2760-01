dafeeb617589469872948ad4f5f038ac9206c1e6
ADAM ANDREASON <adamandreason@gmail.com>
2020-03-26 22:02:34 -0600

    now with EXTRA SARCASM!

--- Capstone/dice.js
+++ Capstone/dice.js
@@ -31,6 +31,11 @@ let challenge = document.querySelector("#greeting");
 const taunt = openerArray[Math.floor(Math.random() * openerArray.length)];
 challenge.textContent = taunt;
 
+function arrayRandomPick(array) {
+  const pick = array[Math.floor(Math.random() * array.length)];
+  return pick;
+}
+
 let numberGen = function(max, min) {
   let number = Math.floor(Math.random() * (max - min)) + min;
   return number;
@@ -67,6 +72,7 @@ let gameStart = document.createElement("button");
 gameStart.textContent = "Let's Play!";
 var currentBet = [0];
 gameBoard.appendChild(gameStart);
+let topInfo = document.querySelector("#topInfo");
 
 //Last action message window
 let statusWindow = document.querySelector("#status");
@@ -222,39 +228,45 @@ function game() {
     if (arrSum(playerTotal) > arrSum(computerTotal)) {
       updateInfo.textContent = "YOU WIN!";
       console.log("Player has won. All is right.");
+      lastWord("player");
       endGame();
     } else {
       updateInfo.textContent = "Computer wins!";
       console.log("Game over. Computer wins.");
+      lastWord("computer");
       endGame();
     }
   } else if (arrSum(playerTotal) > 100) {
     updateInfo.textContent = "YOU WIN!";
     console.log("Player has won. All is right.");
+    lastWord("player");
     endGame();
   } else if (arrSum(computerTotal) > 100) {
     updateInfo.textContent = "Computer wins!";
     console.log("Game over. Computer wins.");
+    lastWord("computer");
     endGame();
   }
 }
 
-function endGame(){
+//Helper function to remove windows
+function endGame() {
   oddevenWindow.remove();
   showDice.remove();
-  gameBoard.style.justifycontent = "space-around";
+  gameBoard.setAttribute("id", "#endBoard");
   let replay = document.createElement("button");
-  replay.textContent = "Play again?"
-  replay.addEventListener('click', () => {
+  replay.textContent = "Play again?";
+  replay.addEventListener("click", () => {
     location.reload();
-  })
+  });
   gameBoard.appendChild(replay);
 }
 
-//youTurn function governs the player's turn, ie.e what happens on odd numbered turns.
+//youTurn function governs the player's turn, i.e. what happens on odd numbered turns.
 function yourTurn() {
   if (turn === 1) {
     gameStart.remove();
+    challenge.style.display = "none";
     computerBet = oddEven();
     message.textContent = `You go first! I bet ${computerBet}.`;
     console.log(`yourTurn says bet is ${computerBet}.`);
@@ -292,6 +304,7 @@ function valueSplicer(bet) {
   }
 }
 
+//Helper function that interprets the array currentBet to get even or odd
 function playerRoll(computerBet) {
   if (arrSum(computerBet) === 1) {
     bet = "odd";
@@ -320,8 +333,8 @@ function rollDice(user, bet) {
     endTurn.style.display = "none";
     oddBet.style.display = "none";
     evenBet.style.display = "none";
-    timedAnimate(user, die1, die2, rollSum);
-    message.textContent = `You rolled snake eyes, so your turn again. I bet ${computerBet}.`
+    timedAnimate(user, die1, die2, rollSum, snake(user));
+    message.textContent = `You rolled snake eyes, so your turn again. I bet ${computerBet}.`;
     computerBet = oddEven();
     snakeHiss.play();
     addtoTotal("player", 2);
@@ -329,7 +342,7 @@ function rollDice(user, bet) {
     oddBet.style.display = "none";
     yourRoll.style.display = "block";
   } else if (die1 === 1 && die2 === 1 && user == "computer") {
-    timedAnimate(user, die1, die2, rollSum);
+    timedAnimate(user, die1, die2, rollSum, snake(user));
     message.textContent = `Rolled snake eyes, baby. I go again.`;
     snakeHiss.play();
     // updateBox(user, die1, die2, rollSum);
@@ -337,21 +350,18 @@ function rollDice(user, bet) {
     contButton.style.display = "none";
     oddBet.style.display = "none";
     evenBet.style.display = "none";
-    // message.textContent = `Rolled snake eyes, so my turn again! Choose your bet.`;
     addtoTotal("player", 2);
     evenBet.style.display = "block";
     oddBet.style.display = "block";
   } else if (user === "player") {
-    let say = "Test message here."
-    timedAnimate(user, die1, die2, rollSum);
+    timedAnimate(user, die1, die2, rollSum, snark(user, rollSum));
     addtoTotal(user, rollSum);
     betCheck("computer", bet, rollSum);
     // updateTotals();
     // updateBox(user, die1, die2, rollSum);
     return [die1, die2, rollSum];
   } else if (user === "computer") {
-    let say = "Test message here."
-    timedAnimate(user, die1, die2, rollSum);
+    timedAnimate(user, die1, die2, rollSum, snark(user, rollSum));
     addtoTotal(user, rollSum);
     betCheck("player", bet, rollSum);
     // updateTotals();
@@ -360,7 +370,7 @@ function rollDice(user, bet) {
   }
 }
 
-//check if you or computer bet correctly, and award points accordingly
+//Checks if you or computer bet correctly, and awards points accordingly
 function betCheck(better, bet, roll) {
   checkRoll = isEven(roll);
   console.log(`The bet is ${bet} for the ${better}.`);
@@ -404,14 +414,7 @@ function computerTurn() {
   oddBet.style.display = "block";
 }
 
-function turnMessage() {
-  if (isEven(turn) === false) {
-    message.textContent = "YOUR TURN";
-  } else {
-    (message.textContent = "COMPUTER'S TURN"), 2000;
-  }
-}
-
+//Some basic helper functions, like adding to the correct user's total, and updating other visable info/the log
 function addtoTotal(user, amount) {
   if (user === "player") {
     playerTotal.push(amount);
@@ -441,7 +444,7 @@ function checkTotal(user) {
   if (user > 100) return true;
 }
 
-//animation for dice
+//General animation function to call
 
 function animate({ timing, draw, duration }) {
   let start = performance.now();
@@ -462,7 +465,8 @@ function animate({ timing, draw, duration }) {
   });
 }
 
-function timedAnimate(user, die1, die2, rollSum) {
+//Function that takes dice and user and animated roll and display of info
+function timedAnimate(user, die1, die2, rollSum, compMessage) {
   animate({
     duration: 900,
     timing(timeFraction) {
@@ -474,6 +478,7 @@ function timedAnimate(user, die1, die2, rollSum) {
         secondDie.src = `/Capstone/assets/${dieImgCall(die2)}`;
         updateBox(user, die1, die2, rollSum);
         message.style.display = "block";
+        topInfo.textContent = compMessage;
         updateLog();
         updateTotals();
       } else {
@@ -484,9 +489,187 @@ function timedAnimate(user, die1, die2, rollSum) {
       }
     }
   });
-
 }
 
+//Arrays for comments based on rolls
+
+let goodRoll = [
+  "Lucky this time. Won't happen next time.",
+  "Well, that was better than you usually do.",
+  "Not bad. But not great.",
+  "I've seen better.",
+  "A normal roll. Nothing to see here.",
+  "*YAWN* Is it my turn yet?",
+  "Wowza. What barn burner. *sarcasm intensifies*",
+  "Normal roll. Move along, nothing to see here.",
+  "K, nothing to get excited about, friend. It's just a roll.",
+  "Its a roll. Whoop-de-doo.",
+  "Huh, what? You still haven't finished your turn? I don't have all day, senior.",
+  "Ho hum. It is what it is.",
+  "Well, that was fine, I guess. For you."
+];
+
+let greatRoll = [
+  "Cheat. You are a cheat.",
+  "Oh, and I supposed you want me to beleive you rolled that legitimately?",
+  "Good for you. Not.",
+  "Well, la di freakin' da. You're still ugly.",
+  "I suppose you feel good about yourself. It won't last.",
+  "Am I supposed to be impressed? This is a game of chance, dude.",
+  "Son of a--",
+  "Congrats. What, do you want a party or something?",
+  "Stop that, will you?!",
+  "Oh, really? Come one, give me a break...",
+  "Well, ain't you a lucky, lucky, sucky duck."
+];
+
+let badRoll = [
+  "Ha! Loser.",
+  "Well, that was pathetic. My turn.",
+  "I'm sorry, do you need some help? I can spot you a point if you ask nicely enough.",
+  "Ain't gonna win that way, chum.",
+  "Welp, THAT didn't work.",
+  "Ha! Sucker.",
+  "Wow. That was terrible. Like, really.",
+  "Um, you do know you want HIGH roll, don't you?",
+  "No problem, you'll get the next. Or the one after. Or the... well, it was a good effort, anyway.",
+  "Well, that was almost a good roll. Almost. Not really.",
+  "*snerk* Good job, good effort...",
+  "Missed it by thaaaat much.",
+  "Juuuuust a bit outside."
+];
+
+let goodCompRoll = [
+  "Ha! Eat my dust!",
+  "Booyah! Eat it!",
+  "I. am. on. a. ROLL!",
+  "Can't stop this train, baby.",
+  "Ain't nohtin' gonna break-a my stride, Nobody gonna slow me down, oh no...",
+  "Hot dog. We rollin' now.",
+  "Boom, baby.",
+  "You could cook an egg on me, cuz I am HOT!",
+  "Mmm mmm. So, SO good.",
+  "I like it a lot."
+];
+
+let badCompRoll = [
+  "What? That can't be right.",
+  "Fine. Just you wait.",
+  "My hand slipped. Reroll? No?",
+  "Hey, me on a bad roll is like you on a great one.",
+  "@#$%! And I mean it!",
+  "Hey kid, don't breath so close to me, you're throwing off my mojo.",
+  "Okay, I admit it, that just sucked."
+];
+
+let winQuote = [
+  "Aww, you thought you could win? That's just cute.",
+  "I. Am. The. GREATEST!",
+  "Better luck next time, Runner Up.",
+  "Well, that turned out exactly as I predicted.",
+  "You just got owned. Come back for more?",
+  "Well, you know what they say. In this life there are winners, and then there is you.",
+  "If I had a body, I'd be breathing on my fingernails and buffing them on my shirt.",
+  "Compuer wins. You lose. Computer wins. You lose. Computer wins. You LOOOOOOSE.",
+  "I'd like to thank all the little people. Like you.",
+  "Second place. First loser."
+];
+
+let loseQuote = [
+  "WHAT?! NO WAY!",
+  "A lucky round, jerk. I bet you can't beat me a second time.",
+  "So what? So you won a game. Even a blind pig find an acorn once in a while. Let's see you repeat it.",
+  "Fine. So you won. Big deal. It's not like I care.",
+  "Oh, COME ON! There's no way I could lose like that!",
+  "Fine. You win. That makes it like a few for you and a billion for me.",
+  "I'll get you next time, human. Next time.",
+  "INCON-- INCONS-- NOT POSSIBLE! How could I lose?!",
+  "ARRRRR....! Fine. Fine. You win. There, I said it.",
+  "YOU--! THAT--! IT--! ...Oh, fine. FINE. But you're STILL ugly!",
+  "NOOOOOOOOOOOOOOO!"
+];
+
+let playerSnake = [
+  "Yeah, you get to roll again, but I get to bet again. And we both know who's better at what.",
+  "Alright, fine, just roll again already.",
+  "Lucky roll, hotshot. Better not waste it.",
+  "Mmmkay. You get another roll. You do know how this works, right?",
+  "And there was great rejoicing. Yay. Now roll again already.",
+  "Is that your roll or your IQ? Just roll again already.",
+  "Slow clap. Now roll agian.",
+  "Don't strain you arm patting yourself on the back. This is a game of chance, you know."
+];
+
+let compSnake = [
+  "And I'm goin' again, I'm goin' again...",
+  "Snake eyes, baby.",
+  "Hiss hiss.",
+  "S-s-s-s-s-SNAKE EYES!",
+  "See the ones, roll again. Gonna roll right over your, friend.",
+  "Sweetness. Ready for the next one.",
+  "And I did that one left-handed. Watch what happens when I do it with my right."
+];
+
+function snark(user, sum) {
+  if (user === "player") {
+    switch (sum) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+        return arrayRandomPick(badRoll);
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+        return arrayRandomPick(goodRoll);
+      case 10:
+      case 11:
+      case 12:
+        return arrayRandomPick(greatRoll);
+    }
+  } else {
+    switch (sum) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+        return arrayRandomPick(badCompRoll);
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+      case 11:
+      case 12:
+        return arrayRandomPick(goodCompRoll);
+    }
+  }
+}
+
+function snake(user) {
+  if (user === "player") {
+    let words = arrayRandomPick(playerSnake);
+    topInfo.textContent = words;
+  } else {
+    let words = arrayRandomPick(compSnake);
+    topInfo.textContent = words;
+  }
+}
+
+function lastWord(winner) {
+  if (winner === "player") {
+    let words = arrayRandomPick(loseQuote);
+    topInfo.textContent = words;
+  } else {
+    let words = arrayRandomPick(winQuote);
+    topInfo.textContent = words;
+  }
+}
+
+//use switch statment and random function to select statements based on roll sum.
 
 //Possible ideas: have it edit a text file to keep track of its global wins and losses?
 //Have serious mode where it admits defeat if you beat it three times in a row?
